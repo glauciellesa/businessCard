680e51988870fa629bfb48418ca0f8668dbea493
Glaucielle Sá <65506911+glauciellesa@users.noreply.github.com>
2022-12-19 15:35:10 +0100

    Set up of skills percentage

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,3 +1,7 @@
+<script setup>
+import SkillsData from "./SkillsData.vue";
+</script>
+
 <template>
   <div class="body">
     <h3>About</h3>
@@ -6,50 +10,14 @@
       I'm passionate about technology and self-taught.
     </p>
     <h3>Skills</h3>
-    <div>
-      <h4 class="subtitle">Frontend developer</h4>
-      <span class="skills_subtitle">1 years of experience</span>
-    </div>
+    <span class="skills_experience">1 years of experience</span>
+
     <div class="skills_list">
-      <div class="skills_data">
-        <div class="skills_titles">
-          <h3 class="skills_name">JavaScript</h3>
-          <span class="skills_number">60%</span>
-        </div>
-        <div class="skills_bar">
-          <span class="skills_percentage skills_javascript"></span>
-        </div>
-      </div>
-
-      <div class="skills_data">
-        <div class="skills_titles">
-          <h3 class="skills_name">CSS</h3>
-          <span class="skills_number">50%</span>
-        </div>
-        <div class="skills_bar">
-          <span class="skills_percentage skills_css"></span>
-        </div>
-      </div>
-
-      <div class="skills_data">
-        <div class="skills_titles">
-          <h3 class="skills_name">HTML</h3>
-          <span class="skills_number">50%</span>
-        </div>
-        <div class="skills_bar">
-          <span class="skills_percentage skills_html"></span>
-        </div>
-      </div>
-
-      <div class="skills_data">
-        <div class="skills_titles">
-          <h3 class="skills_name">Node</h3>
-          <span class="skills_number">40%</span>
-        </div>
-        <div class="skills_bar">
-          <span class="skills_percentage skills_node"></span>
-        </div>
-      </div>
+      <SkillsData skillsName="JavaScript" percentage="70%"></SkillsData>
+      <SkillsData skillsName="CSS" percentage="50%"></SkillsData>
+      <SkillsData skillsName="HTM" percentage="60%"></SkillsData>
+      <SkillsData skillsName="Node" percentage="50%"></SkillsData>
+      <SkillsData skillsName="React" percentage="40%"></SkillsData>
     </div>
   </div>
 </template>
@@ -66,79 +34,13 @@ h3 {
 p {
   font-size: 0.8em;
 }
-.subtitle {
-  margin: 10px 05px;
-}
-.skills_subtitle {
+.skills_experience {
   font-size: 0.8em;
   font-weight: 100;
-  color: #d2d2d2;
+  color: #f3bf99;
   margin: 5px;
-}
-.skills_titles {
-  display: flex;
-  justify-content: space-between;
-  padding-bottom: 10px;
 }
 .skills_list {
   padding: 1rem;
-}
-
-.skills_data {
-  margin-bottom: 0.5em;
-}
-.skills_number {
-  display: flex;
-  align-items: flex-end;
-}
-.skills_number,
-.skills_name {
-  font-size: 1em;
-  font-weight: 200;
-}
-
-.skills_bar,
-.skills_percentage {
-  height: 5px;
-  border-radius: 0.25rem;
-}
-
-.skills_bar {
-  background-color: hsl(20, 92%, 85%);
-}
-
-.skills_percentage {
-  display: block;
-  background-color: hsl(20, 69%, 61%);
-}
-.skills_javascript {
-  width: 60%;
-}
-.skills_css {
-  width: 50%;
-}
-.skills_html {
-  width: 50%;
-}
-.skills_node {
-  width: 40%;
-}
-.skills_esnext {
-  width: 10%;
-}
-.skills_ajax {
-  width: 10%;
-}
-.skills_bootstrap {
-  width: 10%;
-}
-.skills_vuejs {
-  width: 10%;
-}
-.skills_jquery {
-  width: 10%;
-}
-.skills_angular {
-  width: 10%;
 }
 </style>
---
+++ src/components/SkillsData.vue
@@ -0,0 +1,85 @@
+<script setup>
+defineProps({
+  skillsName: String,
+  percentage: String,
+});
+</script>
+
+<template>
+  <div class="skills_data">
+    <div class="skills_titles">
+      <h3 class="skills_name">{{ skillsName }}</h3>
+      <span class="skills_number">{{ percentage }}</span>
+    </div>
+    <div class="skills_bar">
+      <span class="skillPercentage"></span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.skills_titles {
+  display: flex;
+  justify-content: space-between;
+  padding-bottom: 10px;
+}
+
+.skills_data {
+  margin-bottom: 0.5em;
+}
+.skills_number {
+  display: flex;
+  align-items: flex-end;
+}
+.skills_number,
+.skills_name {
+  font-size: 1em;
+  font-weight: 200;
+}
+
+.skills_bar,
+.skillPercentage {
+  height: 5px;
+  border-radius: 0.25rem;
+}
+
+.skills_bar {
+  background-color: hsl(20, 92%, 85%);
+}
+
+.skillPercentage {
+  display: block;
+  background-color: hsl(20, 69%, 61%);
+  width: v-bind("percentage");
+}
+/* .skills_javascript {
+  width: 60%;
+}
+.skills_css {
+  width: 50%;
+}
+.skills_html {
+  width: 50%;
+}
+.skills_node {
+  width: 40%;
+}
+.skills_esnext {
+  width: 10%;
+}
+.skills_ajax {
+  width: 10%;
+}
+.skills_bootstrap {
+  width: 10%;
+}
+.skills_vuejs {
+  width: 10%;
+}
+.skills_jquery {
+  width: 10%;
+}
+.skills_angular {
+  width: 10%;
+} */
+</style>
